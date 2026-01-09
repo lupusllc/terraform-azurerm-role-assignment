d9f54edff20063823eb670e3143e6746dcae3eff
Canis Lupus <canis.lupus@austin.utexas.edu>
2026-01-09 09:31:51 -0600

    Locals standards changes

--- locals.tf
+++ locals.tf
@@ -2,47 +2,48 @@
 
 locals {
   role_assignments_list = [
-    for index, settings in var.role_assignments : {
-      # Most will try and use key/value settings first, then try applicable defaults and then null as a last resort.
+    for index, role_assignment in var.role_assignments : {
+      # Most will try and use key/value role_assignment first, then try applicable defaults and then null as a last resort.
       ### Basic
 
-      condition                              = settings.condition
-      condition_version                      = settings.condition_version
-      delegated_managed_identity_resource_id = settings.delegated_managed_identity_resource_id
-      description                            = settings.description
+      condition                              = role_assignment.condition
+      condition_version                      = role_assignment.condition_version
+      delegated_managed_identity_resource_id = role_assignment.delegated_managed_identity_resource_id
+      description                            = role_assignment.description
       index                                  = index # Added in case it's ever needed, since for_each/for loops don't have inherent indexes.
-      name                                   = settings.name
-      principal_id                           = settings.principal_id
-      principal_type                         = settings.principal_type
-      role_definition_id                     = settings.role_definition_id
-      role_definition_name                   = settings.role_definition_name
-      scope                                  = settings.scope
-      scope_name                             = try(split("/", settings.scope)[8], null) # Split out name to populate scope_name, if it's just a resource group scope it will be null.
-      scope_resource_group_name              = try(split("/", settings.scope)[4], null) # Split out resource group name to populate scope_resource_group_name.
-      skip_service_principal_aad_check       = settings.skip_service_principal_aad_check
-      unique_for_each_id                     = settings.unique_for_each_id
+      name                                   = role_assignment.name
+      principal_id                           = role_assignment.principal_id
+      principal_type                         = role_assignment.principal_type
+      role_definition_id                     = role_assignment.role_definition_id
+      role_definition_name                   = role_assignment.role_definition_name
+      scope                                  = role_assignment.scope
+      scope_name                             = try(split("/", role_assignment.scope)[8], null) # Split out name to populate scope_name, if it's just a resource group scope it will be null.
+      scope_resource_group_name              = try(split("/", role_assignment.scope)[4], null) # Split out resource group name to populate scope_resource_group_name.
+      skip_service_principal_aad_check       = role_assignment.skip_service_principal_aad_check
+      unique_for_each_id                     = role_assignment.unique_for_each_id
     }
   ]
 
   # Used to create unique id for for_each loops, as just using the name may not be unique.
   role_assignments = {
+    # If unique_for_each_id is not null, use it directly.
+    for role_assignment in local.role_assignments_list : role_assignment.unique_for_each_id != null ? role_assignment.unique_for_each_id :
     # If scope_name is null, use resource group name, principal ID and role definition name or ID, if null, as the unique ID.
-    for index, settings in local.role_assignments_list : settings.unique_for_each_id != null ? settings.unique_for_each_id :
-    settings.scope_name == null ?
+    role_assignment.scope_name == null ?
     format(
       "%s>%s>%s",
-      settings.scope_resource_group_name,
-      settings.principal_id,
-      coalesce(settings.role_definition_name, settings.role_definition_id)
+      role_assignment.scope_resource_group_name,
+      role_assignment.principal_id,
+      coalesce(role_assignment.role_definition_name, role_assignment.role_definition_id)
     ) :
     # Otherwise, use resource group name, resource name, principal ID and role definition name or ID, if null, as the unique ID.
     format(
       "%s>%s>%s>%s",
-      settings.scope_resource_group_name,
-      settings.scope_name,
-      settings.principal_id,
-      coalesce(settings.role_definition_name, settings.role_definition_id)
+      role_assignment.scope_resource_group_name,
+      role_assignment.scope_name,
+      role_assignment.principal_id,
+      coalesce(role_assignment.role_definition_name, role_assignment.role_definition_id)
     )
-    => settings
+    => role_assignment
   }
 }
